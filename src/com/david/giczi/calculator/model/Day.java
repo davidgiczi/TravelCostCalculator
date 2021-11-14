c6d4ac9f5dbe93dbadd301a6e5efbfda35508c64
david.giczi@gmail.com <david.giczi@gmail.com>
2021-11-14 18:32:06 +0100

    second commit

---
+++ src/com/david/giczi/calculator/model/Day.java
@@ -0,0 +1,23 @@
+package com.david.giczi.calculator.model;
+
+public class Day {
+
+	
+	private boolean isWorkDay;
+	private int numberOfMonth;
+	
+	
+	public boolean isWorkDay() {
+		return isWorkDay;
+	}
+	public void setWorkDay(boolean isWorkDay) {
+		this.isWorkDay = isWorkDay;
+	}
+	public int getNumberOfMonth() {
+		return numberOfMonth;
+	}
+	public void setNumberOfMonth(int numberOfMonth) {
+		this.numberOfMonth = numberOfMonth;
+	}
+	
+}
